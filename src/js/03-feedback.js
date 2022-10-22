033ef8d26473a7ec89d8758112d8d1ebdabfedbf
Bohatynska Oksana <98977553+Bohatynska@users.noreply.github.com>
2022-10-22 23:45:04 +0100

    add 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
 import throttle from 'lodash.throttle';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form  textarea'),
+  input: document.querySelector('.feedback-form input'),
+};
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+
+const STORAGE_KEY = 'feedback-form-state';
+
+isTextarea();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  if (email.value === '' || message.value === '') {
+    return alert('Все поля должны быть заполнены!');
+  }
+}
+
+function onTextareaInput(event) {
+  const message = event.target.value;
+  localStorage.setItem(STORAGE_KEY, message);
+}
+
+// ============проверка ==============//
+function isTextarea() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  //   if (savedMessage) {
+  //     refs.textarea.value = savedMessage;
+  //   }
+}

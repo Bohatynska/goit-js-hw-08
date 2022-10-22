e7d4adf950d01c6c453cd1040b05f34b7ce61c4a
Bohatynska Oksana <98977553+Bohatynska@users.noreply.github.com>
2022-10-22 18:51:00 +0100

    add tasks 1.2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// console.log(galleryItems);
+const galleryListRef = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItems(galleryItems);
 
-console.log(galleryItems);
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </div>`;
+    })
+
+    .join('');
+}
+galleryListRef.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryListRef.addEventListener('click', onGalleryListClick);
+
+function onGalleryListClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
+const iframeRef = document.querySelector('iframe');
+const player = new Player(iframeRef);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.setCurrentTime(+localStorage.getItem(STORAGE_KEY));
+// player.setCurrentTime(localStorage.getItem(STORAGE_KEY) || 0);
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+function onTimeUpdate(params) {
+  localStorage.setItem(STORAGE_KEY, params.seconds);
+}

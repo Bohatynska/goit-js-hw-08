25a707bf38139c62c90058f9432c108ef3f1f024
Bohatynska Oksana <98977553+Bohatynska@users.noreply.github.com>
2022-10-24 20:18:34 +0100

    fixed 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -12,24 +12,26 @@ refs.form.addEventListener('input', throttle(onInput, 500));
 const STORAGE_KEY = 'feedback-form-state';
 const formData = {};
 
+isTextarea();
+
 function onFormSubmit(event) {
   event.preventDefault();
   event.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
+  console.log(formData);
 }
 
 function onInput(event) {
   formData[event.target.name] = event.target.value;
-  // console.log(formData);
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 // ============проверка ==============//
-isTextarea();
+
 function isTextarea() {
   const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
-  console.log(savedMessage.email);
-  console.log(savedMessage.message);
+  console.log(savedMessage?.email);
+  console.log(savedMessage?.message);
   console.log(savedMessage);
   if (savedMessage) {
     refs.email.value = savedMessage.email;
